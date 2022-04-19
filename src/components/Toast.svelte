7e05f7e139ed844ffac5b3b9e9da8f53bc018e7f
davidfrp <davidfrp@outlook.dk>
2022-04-19 20:47:38 +0200

    Added toast component for on-screen notifications

---
+++ src/components/Toast.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { slide } from 'svelte/transition';
+
+    export let message;
+    export let isShown;
+
+    const onDismiss = () => isShown = false;
+</script>
+
+{#if isShown}
+    <div class="toast" role="status" transition:slide>
+        <p>{@html message}</p>
+        <button type="button" on:click={onDismiss}>dismiss</button>
+    </div>
+{/if}
+
+<style>
+    .toast {
+        padding: 1.25em;
+        margin: 1.5em 0;
+        background-color: #fff4f8;
+        display: flex;
+        gap: 1em;
+    }
+
+    p {
+        font-size: 0.75em;
+        margin: 0;
+    }
+
+    button {
+        font-size: 0.8em;
+        cursor: pointer;
+        color: #9f4c69;
+        background: none;
+        width: auto;
+        padding: 0;
+        margin: 0;
+    }
+</style>
